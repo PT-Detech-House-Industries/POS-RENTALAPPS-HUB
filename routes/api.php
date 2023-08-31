b58884e882ea56570e46f2f107138ec1dca0cfb5
Edward Kristian Mangare, S.Kom <37824502+krizedward@users.noreply.github.com>
2023-08-31 13:46:00 +0700

    update 1.1.3

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TalentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use App\Http\Controllers\API\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// talent
+Route::get('/talent', [TalentController::class, 'index']);
 
 // auth
 Route::post('/login', [AuthController::class, 'login']);
